441efa77a6077fac7bf9feafb10a31b2801bae6f
Christine Labranche <2490670@cegepmontpetit.ca>
2026-03-06 16:20:31 -0500

    PROGRES : Question 3 ReactiveForms
    La plupart des validations fonctionnent mais pas celles pour le code postal ni pour le formulaire au complet

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,11 +18,40 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 export class AppComponent {
   title = 'reactive.form';
 
-  
+  formGroup: FormGroup;
 
   constructor(
-    
-  ) { }
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group(
+      {
+        name: ['', [Validators.required]],
+        roadnumber: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
+        postalcode: ['', []],
+        comments: ['', [Validators.minLength(10)]]
+      },
+      {
+        validators: nomDansCommentaire()
+      }
+    );
+  }
+}
+
+
+export function nomDansCommentaire(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const comments = control.value;
+    const name = control.value;
+    // On regarde si le champ est rempli avant de faire la validation
+    if (!comments?.value || !name?.value) {
+      // On attend que le champ soit rempli avant de le valider
+      return null;
+    }
+    // On fait notre validation. Includes retourne un booléen.
+    const estValide = !comments.value.includes(name.value);
+
+    return estValide ? null : { nomDansCommentaire: true }
+  };
 }
 
 
